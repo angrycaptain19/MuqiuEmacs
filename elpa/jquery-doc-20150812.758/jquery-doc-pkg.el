4eb224594e409a0ed824990154f5bf3c0c6c3c68
韩暮秋 <muqiu-han@outlook.com>
2021-01-12 17:46:41 +0800

    暮秋小屋Emacs

---
+++ elpa/jquery-doc-20150812.758/jquery-doc-pkg.el
@@ -0,0 +1,9 @@
+(define-package "jquery-doc" "20150812.758" "jQuery api documentation interface for emacs" 'nil :commit "24032284919b942ec27707d929bdd8bf48420062" :authors
+  '(("Anantha kumaran" . "ananthakumaran@gmail.com"))
+  :maintainer
+  '("Anantha kumaran" . "ananthakumaran@gmail.com")
+  :keywords
+  '("docs" "jquery"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/spiral-20180223.1140/spiral-pkg.el
@@ -0,0 +1,17 @@
+(define-package "spiral" "20180223.1140" "Clojure IDE based on UNREPL"
+  '((emacs "25.1")
+    (a "0.1.0alpha4")
+    (avy "0.4.0")
+    (clojure-mode "5.6.0")
+    (highlight "0")
+    (treepy "1.0.0"))
+  :commit "907b9792467139a942ba7b07ca0276b90770baf9" :authors
+  '(("Daniel Barreto" . "daniel@barreto.tech"))
+  :maintainer
+  '("Daniel Barreto" . "daniel@barreto.tech")
+  :keywords
+  '("languages" "clojure")
+  :url "https://github.com/Unrepl/spiral")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
