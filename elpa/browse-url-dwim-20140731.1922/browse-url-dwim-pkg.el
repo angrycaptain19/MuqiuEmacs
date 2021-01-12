47291513eb25f999d9ae0e70ca8534e4a73f3b89
韩暮秋 <muqiu-han@outlook.com>
2021-01-12 17:56:38 +0800

    暮秋小屋Emacs

--- elpa/browse-url-dwim-20140731.1922/browse-url-dwim-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/muqiu/.emacs.d/elpa/browse-url-dwim-20140731.1922/browse-url-dwim.el  -*- no-byte-compile: t -*-
-(define-package "browse-url-dwim" "20140731.1922" "Context-sensitive external browse URL or Internet search" '((string-utils "0.3.2")) :commit "3d611dbb167c286109ac53995ad68286d87aafb9" :authors '(("Roland Walker" . "walker@pobox.com")) :maintainer '("Roland Walker" . "walker@pobox.com") :keywords '("hypermedia") :url "http://github.com/rolandwalker/browse-url-dwim")
--- elpa/osx-browse-20140508.2041/osx-browse-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/muqiu/.emacs.d/elpa/osx-browse-20140508.2041/osx-browse.el  -*- no-byte-compile: t -*-
-(define-package "osx-browse" "20140508.2041" "Web browsing helpers for OS X" '((string-utils "0.3.2") (browse-url-dwim "0.6.6")) :commit "44ded7cc3a7ee426c1c3257fae534c121f7e752e" :authors '(("Roland Walker" . "walker@pobox.com")) :maintainer '("Roland Walker" . "walker@pobox.com") :keywords '("hypermedia" "external") :url "http://github.com/rolandwalker/osx-browse")
